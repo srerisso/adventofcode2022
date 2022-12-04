d1e16e5711f227ba2ac6729c8a3a073ad7508c3d
Sr erisso <srerisso@users.noreply.github.com>
2022-12-04 01:15:55 +0100

    Day 2 finished.

--- day2/day2.rb
+++ day2/day2.rb
@@ -15,7 +15,7 @@ file_data.each do |el|
     new_file_data.push((el.gsub(/\s+/, "")))
 end
 
-print(new_file_data)
+# print(new_file_data)
 
 # Win tuples: AY, BZ, CX
 # Draw tuples : AX, BY, CZ
@@ -60,15 +60,65 @@ new_file_data.each do |el|
     end
 end
 
-print(sum)
-
+puts("SUM is ", sum)
 
 # PART 2.
+#  Rock     A
+#  Paper    B
+#  Scissors C 
+
+# score_outcome
+# Win   (Z) 6
+# Draw  (Y) 3
+# Loss  (X) 0
+# 
+# score_shape
+# Rock      (A) 1
+# Paper     (B) 2
+# Scissors  (C) 3
+
 # "X means you need to lose, 
 # Y means you need to end the round in a draw, 
 # and Z means you need to win. Good luck!"
 # 
+# A > C > B > A
+# Rock > Scissors > Paper > Rock
+# 
 # what would your total score be if everything goes exactly according to your strategy guide?
-# A Y => X
-# B X => Z
-# C Z => X
+# A X => C, score = 0 + 3
+# A Y => A, score = 3 + 1
+# A Z => B, score = 6 + 2
+# B X => A, score = 0 + 1
+# B Y => B, score = 3 + 2
+# B Z => C, score = 6 + 3
+# C X => B, score = 0 + 2
+# C Y => C, score = 3 + 3
+# C Z => A, score = 6 + 1
+
+
+sum2=0
+new_file_data.each do |el|
+    case el
+    when "AX"
+        sum2 = sum2 + 3
+    when "AY"
+        sum2 = sum2 + 4
+    when "AZ"
+        sum2 = sum2 + 8
+    when "BX"
+        sum2 = sum2 + 1
+    when "BY"
+        sum2 = sum2 + 5
+    when "BZ"
+        sum2 = sum2 + 9
+    when "CX"
+        sum2 = sum2 + 2
+    when "CY"
+        sum2 = sum2 + 6
+    when "CZ"
+        sum2 = sum2 + 7
+    end
+end
+
+puts("SUM2 is ", sum2)
+
