de6ce2d7036653e9a6bfc5838e62fa294dc151a8
Sr erisso <srerisso@users.noreply.github.com>
2022-12-09 21:01:13 +0100

    Day 4 finished.

--- day4/day4.rb
+++ day4/day4.rb
@@ -6,7 +6,7 @@ file_data = file.readlines.map(&:chomp)
 # In how many assignment pairs does one range fully contain the other?
 
 # Function translateRange: translate Integer1-Integer2 to Array[*(Integer1..Integer2)]
-def translateRange(str)
+def translateRange str
     if !(str.empty?)
         (r1,r2)=str.split('-')
         Array[*(r1..r2)]
@@ -21,7 +21,6 @@ class Array
 end
 
 total=0
-totalArray = []
 
 file_data.each do |el|
     (range1,range2) = el.split(",")
@@ -30,8 +29,27 @@ file_data.each do |el|
     if ra1.included_in?(ra2) || ra2.included_in?(ra1)
         total += 1
     end
-    #  totalArray.push(ra1)
-    printf("*** R1 is %s \n *** R2 is %s. \n TOTAL: %d \n\n", ra1,ra2,total)
+    # printf("*** R1 is %s \n *** R2 is %s. \n TOTAL: %d \n\n", ra1,ra2,total)
 end
 
-printf("Total assignment pairs fully containing the other:%d ", total)
+printf("Total assignment pairs fully containing the other: %d \n", total)
+
+# ****************************************************************
+# ********* PART 2
+# How many pairs OVERLAP ???
+
+print("****** PART 2 ******")
+total2=0
+file_data.each do |el|
+    (range1,range2) = el.split(",")
+    ra1 = translateRange(range1)
+    ra2 = translateRange(range2)
+    if ra1.any? { |el| ra2.include?(el) }
+        total2 += 1
+    end
+    # printf("++ RA1: %s \n",ra1)
+    # printf("-- RA2: %s \n\n",ra2)
+    printf("*** R1 is %s \n *** R2 is %s. \n TOTAL: %d \n\n", ra1,ra2,total2)
+end
+
+printf("Total assignment pairs overlapping in some part: %d \n", total2)
