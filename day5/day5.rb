416ee3d828d090e528c151816ddcf660fdca2f04
Sr erisso <srerisso@users.noreply.github.com>
2022-12-11 16:34:04 +0100

    Day 5 start.

---
+++ day5/day5.rb
@@ -0,0 +1,16 @@
+# --- Day 5: Supply Stacks ---
+file = File.open("day5_input.txt")
+file_data = file.read
+
+# After the rearrangement procedure completes, what crate ends up on top of each stack?
+print(file_data)
+
+# file_data.split
+
+# Read File in a String.
+# \n separates stacks and movements.
+
+# Reg. expressions ??
+# stacks:
+# crates moves : move number from number to number
+
